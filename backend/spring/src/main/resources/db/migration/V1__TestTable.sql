15624ad625d70698cd934ee341bd06d2a8e2388b
Evan Byrnes <evan.byrnes@ucdconnect.ie>
2021-02-05 13:24:28 +0000

    more gitignore

---
+++ backend/spring/src/main/resources/db/migration/V1__TestTable.sql
@@ -0,0 +1,8 @@
+CREATE TABLE test (
+                countyName VARCHAR(100) NOT NULL PRIMARY KEY,
+                fid INTEGER NOT NULL,
+                confirmedCovidCases INTEGER,
+                populationProportionCovidCases INTEGER,
+                timeStampDate INTEGER,
+                dateString VARCHAR (20)
+);
